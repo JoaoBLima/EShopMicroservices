e0f11b5bb770ea7d4d84393d1be82c2bc628068d
Joao Borges10 <126673387+Joaoborges10@users.noreply.github.com>
2026-01-14 15:32:06 -0300

    Início da implementação do microserviço de catálogo e suas bibliotecas e docker compose configuração.

---
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -0,0 +1,23 @@
+using Carter;
+using Marten;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// add services to the container
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+});
+
+builder.Services.AddMarten(opts =>
+{
+    opts.Connection(builder.Configuration.GetConnectionString("Database")!);
+}).UseLightweightSessions();
+var app = builder.Build();
+
+// Configure the HTTP request pipeline
+
+app.Run();
